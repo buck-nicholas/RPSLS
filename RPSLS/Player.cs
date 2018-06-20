8fe7fc4bea2d0576f90dbaea2b338af1073de14c
buck-nicholas <buck.nicholas.c@gmail.com>
2018-06-20 16:03:24 -0500

    Added Classes: gameSetUp, Player. Added game mode selection. Added ability to get player customized names and create ai instance.

---
+++ RPSLS/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        // member variables
+        public string playerName;
+        public bool humanPlayer; // if humanPlayer, set true, AI set false
+        public int playerScore;
+
+        // constructor
+        public Player(string name, bool ai)
+        {
+            // constructor info
+            playerName = name;
+            humanPlayer = ai;
+            playerScore = 0;
+        }
+        // member methods
+        public void GetPlayerName()
+        {
+            if (humanPlayer)
+            {
+                Console.WriteLine("Please Enter Your Name");
+                playerName = Console.ReadLine();
+                if (playerName.Length == 0)
+                {
+                    Console.WriteLine("You have entered an invalid response, please resubmit utilizing charactors A-Z and or numbers 0-9.");
+                    GetPlayerName();
+                }
+                Console.WriteLine("Welcome to the game " + playerName + ".");
+            }
+        }
+    }
+}
